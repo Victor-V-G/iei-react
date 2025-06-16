9b7570210eed536d73d0d68cb186d442b2b5cba1
vxtxcx <victorvicencio932@gmail.com>
2025-06-16 14:26:25 -0400

    variables y array, simplicar codigo
    ej2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,54 @@
 'use client' //directiva para obligar a que funcione ya que no estamos trabajando en un servidor
 import { useState } from "react";
 
+// interface: define tipo de datos de ciertas variables que deberian estar en un objeto
+interface Persona{
+  nombre : string,
+  apellido : string,
+}
+const initialStatePersona:Persona = {
+  nombre: "",
+  apellido: ""
+}
 
 export default function Home() {
   // first: nombre del estado,
   // setFirst: cambiar el estado
   // " ": string sin nada asignado
   // --dependiendo del tipo de estado inicial indica cual sera el tipo del estado, es decir sera String o Number
-  const [nombre, setNombre] = useState("")
-  const [apellido, setApellido] = useState("")
+  const [persona, setPersona] = useState(initialStatePersona)
+  //handle: manipula algo o una funcion
+  const handlePersona = (name:string,value:string)=>{
+    // validaciones
+    // aqui toma la persona, la desarma y arma los campos
+    // ...persona : recupera lo que tenia previamente
+    // lo que hace es, actualizar el nombre o el apellido dependiendo de su respectivo campo
+    setPersona(
+      {...persona,[name]:value}
+    )
+  }
+
   return (
     <form>
-      <h1>hola {nombre} {apellido}</h1>
+      <h1>hola {persona.nombre} {persona.apellido} </h1>
       <label>Nombre</label><br />
-      <input 
+      <input
+        // name: forma de reconocer una variable o etiqueta
+        name="nombre"
         type="text" 
         placeholder="Nombre"
         // e : representara el evento
+        // e.currenTarget.value : recupera el nombre del valor que se gatillara
         // e.currenTarget.value : recupera el valor desde que se gatillo el evento
-        onChange={(e)=>setNombre(e.currentTarget.value)}/><br />
+        onChange={(e)=>handlePersona(e.currentTarget.name,e.currentTarget.value)}/><br />
       <span></span><br />
 
       <label>Apellido</label><br />
-      <input 
+      <input
+        name="apellido"
         type="text" 
         placeholder="Apellido"
-        onChange={(e)=>setApellido(e.currentTarget.value)}/><br />
+        onChange={(e)=>handlePersona(e.currentTarget.name,e.currentTarget.value)}/><br />
       <span></span><br />
       <button>Registrar</button>
     </form>
