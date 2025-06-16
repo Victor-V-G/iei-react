b5750b7ec87fe1d05e25fd6f6d81596f85e9f72a
vxtxcx <victorvicencio932@gmail.com>
2025-06-16 14:41:22 -0400

    validacion nombre
    ej 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 'use client' //directiva para obligar a que funcione ya que no estamos trabajando en un servidor
 import { useState } from "react";
 
+//npx next dev : para iniciar el programa en el navegador
+
 // interface: define tipo de datos de ciertas variables que deberian estar en un objeto
 interface Persona{
   nombre : string,
   apellido : string,
+  edad : number,
 }
 const initialStatePersona:Persona = {
   nombre: "",
-  apellido: ""
+  apellido: "",
+  edad: 0,
 }
 
 export default function Home() {
@@ -17,6 +21,7 @@ export default function Home() {
   // " ": string sin nada asignado
   // --dependiendo del tipo de estado inicial indica cual sera el tipo del estado, es decir sera String o Number
   const [persona, setPersona] = useState(initialStatePersona)
+  const [eNombre, setENombre] = useState("")
   //handle: manipula algo o una funcion
   const handlePersona = (name:string,value:string)=>{
     // validaciones
@@ -26,11 +31,19 @@ export default function Home() {
     setPersona(
       {...persona,[name]:value}
     )
+
+    // validacion 2
+    // se llama el estado setENombre
+    if (persona.nombre.length < 3) {
+      setENombre("debe tener mas de 3 caracteres")      
+    }else{
+      setENombre("")
+    }
   }
 
   return (
     <form>
-      <h1>hola {persona.nombre} {persona.apellido} </h1>
+      <h1>hola {persona.nombre} {persona.apellido} {persona.edad}</h1>
       <label>Nombre</label><br />
       <input
         // name: forma de reconocer una variable o etiqueta
@@ -41,7 +54,7 @@ export default function Home() {
         // e.currenTarget.value : recupera el nombre del valor que se gatillara
         // e.currenTarget.value : recupera el valor desde que se gatillo el evento
         onChange={(e)=>handlePersona(e.currentTarget.name,e.currentTarget.value)}/><br />
-      <span></span><br />
+      <span>{eNombre}</span><br />
 
       <label>Apellido</label><br />
       <input
@@ -50,7 +63,16 @@ export default function Home() {
         placeholder="Apellido"
         onChange={(e)=>handlePersona(e.currentTarget.name,e.currentTarget.value)}/><br />
       <span></span><br />
+
+      <label>Edad</label><br />
+      <input
+        name="edad"
+        type="number" 
+        placeholder="Edad"
+        onChange={(e)=>handlePersona(e.currentTarget.name,e.currentTarget.value)}/><br />
+      <span></span><br />
       <button>Registrar</button>
+      
     </form>
   );
 }
